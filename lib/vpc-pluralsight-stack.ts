1a0be38f40bfcaf19efdf28ee2208236ffd902ea
Graeme Hosford <graemehosford@gmail.com>
2022-05-30 16:24:45 +0100

    Create public VPC through CDK

--- lib/vpc-pluralsight-stack.ts
+++ lib/vpc-pluralsight-stack.ts
@@ -1,19 +1,58 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import {RemovalPolicy, Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class VpcPluralsightStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'VpcPluralsightQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+        /* Create a VPC to act as public facing part of infra */
+        const publicVpc = new ec2.Vpc(this, "PublicVpc", {
+            vpcName: "web-vpc",
+            cidr: "10.1.0.0/16",
+            // If left undefined CDK tries to create default subnets which causes conflict and errors
+            // Empty array prevents this
+            subnetConfiguration: []
+        });
+        publicVpc.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
-    const topic = new sns.Topic(this, 'VpcPluralsightTopic');
+        /* Create a public subnet which holds the public facing parts of a web app */
+        const publicSubnet = new ec2.Subnet(this, "PublicSubnet", {
+            vpcId: publicVpc.vpcId,
+            availabilityZone: "eu-west-1a",
+            cidrBlock: "10.1.254.0/24"
+        });
+        publicSubnet.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+        /* Create a route table tied to the public VPC */
+        const publicRouteTable = new ec2.CfnRouteTable(this, "PublicRouteTable", {
+            vpcId: publicVpc.vpcId
+        });
+        publicRouteTable.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+        const publicSubnetRouteTableAssociation =
+            new ec2.CfnSubnetRouteTableAssociation(this, "PublicRouteTableSubnetAssociation", {
+                subnetId: publicSubnet.subnetId,
+                routeTableId: publicRouteTable.attrRouteTableId
+            });
+        publicSubnetRouteTableAssociation.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+        /* Create an internet gateway which allow public VPC to access the outside world and vice versa */
+        const publicInternetGateway = new ec2.CfnInternetGateway(this, "PublicInternetGateway");
+        publicInternetGateway.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+        const publicVpcIgwAttachment = new ec2.CfnVPCGatewayAttachment(this, "PublicVpcIgwAttachment", {
+            vpcId: publicVpc.vpcId,
+            internetGatewayId: publicInternetGateway.attrInternetGatewayId
+        });
+        publicVpcIgwAttachment.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+        /* cidr block 0.0.0.0/0 means all IPV4 traffic should get access to internet ingress and egress */
+        const route = new ec2.CfnRoute(this, "PublicInternetAccessRoute", {
+            routeTableId: publicRouteTable.attrRouteTableId,
+            destinationCidrBlock: "0.0.0.0/0",
+            gatewayId: publicInternetGateway.attrInternetGatewayId
+        });
+        route.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    }
 }
